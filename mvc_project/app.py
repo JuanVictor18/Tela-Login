299687c9a234f816e82d51ada48922d799274d5e
Juan <juanbritoleal@gmail.com>
2024-05-02 19:31:45 -0300

    subindo o python

--- mvc_project/app.py
+++ mvc_project/app.py
@@ -1,22 +1,28 @@
 from flask import Flask, render_template, redirect, request, url_for
 from models.user import User
 from controllers.login_controller import login
+
 app = Flask(__name__)
+
 # Rota para a página de login
 @app.route('/')
 def index():
- return render_template('login.html')
+    return render_template('login.html')
+
 # Rota para a página de boas-vindas
 @app.route('/welcome')
 def welcome():
-   return render_template('welcome.html')
+    return render_template('welcome.html')
+
 # Rota para a página de erro
 @app.route('/error')
 def error():
- return render_template('error.html')
+    return render_template('error.html')
+
 # Rota para lidar com a submissão do formulário de login
 @app.route('/login', methods=['POST'])
 def submit_login():
- return login()
+    return login()
+
 if __name__ == '__main__':
- app.run(debug=True)
+    app.run(debug=True)
--- mvc_project/models/user.py
+++ mvc_project/models/user.py
@@ -1,5 +1,4 @@
-# models/user.py
 class User:
- def __init__(self, username, password):
-  self.username = username
-  self.password = password 
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
