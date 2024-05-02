e03e25ed7e77b680063817ef17ebc82abe3159c5
Juan <juanbritoleal@gmail.com>
2024-05-02 18:24:18 -0300

    passando o php

--- mvc-php/model.php
+++ mvc-php/model.php
@@ -1,14 +1,20 @@
-// model.php
 <?php
-class User {
-private $username;
-private $password;
-public function __construct($username, $password) {
-$this->username = $username;
-$this->password = $password;
-}
-public function isValid() {
-return $this->username === 'Admin' && $this->password === '123456';
-}
+class User{
+    private $username;
+    private $password;
+    
+    public function __construct($username, $password)
+    {
+        $this->username = $username;
+        $this->password = $password;
+    
+
+    }
+    public function isValid(){
+        return $this->username === 'Admin' && $this->password ==='123456'; 
+    }
+
+
+
 }
 ?>
